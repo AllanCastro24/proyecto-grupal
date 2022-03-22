ed80b82aafe529029b5bb977d411fc4ada19e37f
MauRobles4 <roblesbalderramamauricio3amp@gmail.com>
2022-03-22 01:01:01 -0700

    reportes de ventas y almacen

---
+++ mox/src/app/admin/reports-sales/reports-sales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-sales',
+  templateUrl: './reports-sales.component.html',
+  styleUrls: ['./reports-sales.component.scss']
+})
+export class ReportsSalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ mox/src/app/admin/reports-stock/reports-stock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-stock',
+  templateUrl: './reports-stock.component.html',
+  styleUrls: ['./reports-stock.component.scss']
+})
+export class ReportsStockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
