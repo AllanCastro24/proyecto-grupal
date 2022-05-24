56b337c482990194acf3db2925641c2516cfc8ac
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-05-24 16:04:08 -0600

    modificar user y cambias pass

--- mox/src/app/pages/account/password-change/password-change.component.ts
+++ mox/src/app/pages/account/password-change/password-change.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { matchingPasswords } from 'src/app/theme/utils/app-validators';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UsersService } from 'src/app/admin/users/users.service';
 
 @Component({
   selector: 'app-password-change',
   templateUrl: './password-change.component.html',
-  styleUrls: ['./password-change.component.scss']
+  styleUrls: ['./password-change.component.scss'],
+  providers: [ UsersService ]  
 })
 export class PasswordChangeComponent implements OnInit {
   public passwordForm!:FormGroup;
   public hide = true; 
-  constructor(public formBuilder: FormBuilder, public snackBar: MatSnackBar) { }
+  constructor(public formBuilder: FormBuilder, public snackBar: MatSnackBar,public usersService:UsersService) { }
 
   ngOnInit(): void {
     this.passwordForm = this.formBuilder.group({
@@ -25,8 +27,12 @@ export class PasswordChangeComponent implements OnInit {
   public onPasswordFormSubmit():void {
     if (this.passwordForm.valid) {
       console.log(this.passwordForm.value)
+      this.cambiar_pass(this.passwordForm.value);
       this.snackBar.open('La contraseña se actualizó con éxito!', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
     }
   }
 
+  public cambiar_pass(user:any){
+    this.usersService.modificarPass(user,user.id).subscribe(l =>{});
+  }
 }
--- mox/src/app/pages/account/profile/profile.component.ts
+++ mox/src/app/pages/account/profile/profile.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { emailValidator } from 'src/app/theme/utils/app-validators';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UsersService } from 'src/app/admin/users/users.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  providers: [ UsersService ]  
 })
 export class ProfileComponent implements OnInit {
   public infoForm!:FormGroup; 
-  constructor(public formBuilder: FormBuilder, public snackBar: MatSnackBar) { }
+  constructor(public formBuilder: FormBuilder, public snackBar: MatSnackBar,public usersService:UsersService) { }
 
   ngOnInit() {
     this.infoForm = this.formBuilder.group({
@@ -24,6 +26,7 @@ export class ProfileComponent implements OnInit {
   public onInfoFormSubmit():void {
     if (this.infoForm.valid) { 
       console.log(this.infoForm.value)
+      this.modificarUser(this.infoForm.value);
       this.snackBar.open('Se actualizó la información de la cuenta correctamente!', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
     }
   } 
@@ -35,5 +38,10 @@ export class ProfileComponent implements OnInit {
     else{
       this.infoForm.controls.image.patchValue(null); 
     }
+  }
+
+  public modificarUser(user:any){
+    console.log("Que tiene "+ user);
+    this.usersService.modificarUser(user,user.id).subscribe();
   }
 }
