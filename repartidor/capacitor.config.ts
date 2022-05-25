9cd8c60603a0ecaac8db4a53ff8d556b10d72413
Joel2B <jbuenouas@gmail.com>
2022-05-25 01:36:54 -0600

    Base del repartidor

---
+++ repartidor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.repartidor.app',
+  appName: 'Repartidor',
+  webDir: 'dist/repartidor',
+  bundledWebRuntime: false
+};
+
+export default config;
